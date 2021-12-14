60fd64dbf57c2c7bf5a2aedb0ffa525868033a38
Paulo de Tasso <paulovendascontato@gmail.com>
2021-12-13 21:28:51 -0300

    13-12-2021 - 2128

--- js/start.js
+++ js/start.js
@@ -16,21 +16,201 @@ function stop() {
 function aumentar_volume() {
 
     var audio = document.getElementById('audio');
+    var icone3 = document.getElementById('iconeSomAumentarVolume')
     
-    if (audio.volume < 1) audio.volume += 0.1;
+    var volumecerto = Math.round(audio.volume*10);
+
+    alert(volumecerto);
+
+    switch (volumecerto) {
+
+        case 10:
+
+            audio.volume += 0.0;
+            icone3.setAttribute('src', '/icones/800x600/volume10.png');
+
+            break;
+
+        case 9:
+
+            audio.volume += 0.1;
+            icone3.setAttribute('src', '/icones/800x600/volume9.png');
+
+            break;
+
+        case 8:
+
+            audio.volume += 0.1;
+            icone3.setAttribute('src', '/icones/800x600/volume8.png');
+
+            break;
+
+        case 7:
+
+            audio.volume += 0.1;
+            icone3.setAttribute('src', '/icone3s/800x600/volume7.png');
+
+            break;
+
+        case 6:
+
+            audio.volume += 0.1;
+            icone3.setAttribute('src', '/icone3s/800x600/volume6.png');
+
+            break;
+
+        case 5:
+
+            audio.volume += 0.1;
+            icone3.setAttribute('src', '/icone3s/800x600/volume5.png');
+
+            break;
+
+        case 4:
+
+            audio.volume += 0.1;
+            icone3.setAttribute('src', '/icone3s/800x600/volume4.png');
+
+            break;
+
+        case 3:
+
+            audio.volume += 0.1;
+            icone3.setAttribute('src', '/icone3s/800x600/volume3.png');
+
+            break;
+
+        case 2:
+
+            audio.volume += 0.1;
+            icone3.setAttribute('src', '/icone3s/800x600/volume2.png');
+
+            break;
+
+        case 1:
+
+            audio.volume += 0.1;
+            icone3.setAttribute('src', '/icone3s/800x600/volume1.png');
+
+            break;
+
+        case 0:
+
+            audio.volume += 0.1;
+            icone3.setAttribute('src', '/icone3s/800x600/volume0.png');
+
+            break;
+
+        default:
+
+            audio.volume += 0.0;
+            icone3.setAttribute('src', '/icone3s/800x600/volumealto0.png');
+
+    }
+
 }
 
 function diminuir_volume() {
 
     var audio = document.getElementById('audio');
+    var icone3 = document.getElementById('iconeSomDiminuirVolume')
 
-    if (audio.volume > 0) audio.volume -= 0.1;
+    var volumecerto = Math.round(audio.volume*10);
+
+    alert(volumecerto);
+
+    switch (volumecerto) {
+
+        case 10:
+
+            audio.volume -= 0.1;
+            icone3.setAttribute('src', '/icones/800x600/volume10.png');
+
+            break;
+
+        case 9:
+
+            audio.volume -= 0.1;
+            icone3.setAttribute('src', '/icones/800x600/volume9.png');
+
+            break;
+
+        case 8:
+
+            audio.volume -= 0.1;
+            icone3.setAttribute('src', '/icones/800x600/volume8.png');
+
+            break;
+
+        case 7:
+
+            audio.volume -= 0.1;
+            icone3.setAttribute('src', '/icone3s/800x600/volume7.png');
+
+            break;
+
+        case 6:
+
+            audio.volume -= 0.1;
+            icone3.setAttribute('src', '/icone3s/800x600/volume6.png');
+
+            break;
+
+        case 5:
+
+            audio.volume -= 0.1;
+            icone3.setAttribute('src', '/icone3s/800x600/volume5.png');
+
+            break;
+
+        case 4:
+
+            audio.volume -= 0.1;
+            icone3.setAttribute('src', '/icone3s/800x600/volume4.png');
+
+            break;
+
+        case 3:
+
+            audio.volume -= 0.1;
+            icone3.setAttribute('src', '/icone3s/800x600/volume3.png');
+
+            break;
+
+        case 2:
+
+            audio.volume -= 0.1;
+            icone3.setAttribute('src', '/icone3s/800x600/volume2.png');
+
+            break;
+
+        case 1:
+
+            audio.volume -= 0.1;
+            icone3.setAttribute('src', '/icone3s/800x600/volume1.png');
+
+            break;
+
+        case 0:
+
+            audio.volume -= 0.1;
+            icone3.setAttribute('src', '/icone3s/800x600/volume0.png');
+
+            break;
+
+        default:
+
+            audio.volume -= 0.0;
+            icone3.setAttribute('src', '/icone3s/800x600/volumealto0.png');
+
+    }
+
 }
 
 function mute() {
     var audio = document.getElementById('audio');
     var icone = document.getElementById('iconeSom')
-   
+
     if (audio.muted) {
         audio.muted = false;
         icone.setAttribute('src', '/icones/sound.png');
