1a6c96b189f996633c1175ebd95bfe901e8fe832
Paulo de Tasso <paulovendascontato@gmail.com>
2021-12-22 18:31:32 -0300

    22-12-2021 - 1831

--- js/TLOD.js
+++ js/TLOD.js
@@ -3,15 +3,19 @@ var portaPrincipal = document.getElementById('portaPrincipal');
 //var numeroAleatorio = Math.random();
 var numeroAleatorio;
 
+var caminho;
+
 function numeroAleatorio(min, max)
 {
     aleatorio = Math.floor(Math.random() * (max - min) + min);
     alert(aleatorio);
 }
 
-function abrir()
+function abrir(aleatorio)
 {
-    portaPrincipal.setAttribute('src', '../imagens/doors/door02.jpg');
-
+    portaPrincipal.setAttribute('src', '../imagens/doors/door' + aleatorio + '.jpg');
 }
 
+caminho = '../imagens/doors/door' + aleatorio + '.jpg';
+
+alert(caminho);
