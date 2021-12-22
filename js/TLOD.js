a949eed1097124c6bc3172e203b662af19f0f6ff
Paulo de Tasso <paulovendascontato@gmail.com>
2021-12-22 19:00:38 -0300

    22-12-2021 - 1900

--- js/TLOD.js
+++ js/TLOD.js
@@ -1,8 +1,8 @@
-var portaPrincipal = document.getElementById('portaPrincipal');
-
 var aleatorio;
 
 var caminho;
+
+var portaPrincipal = document.getElementById('portaPrincipal');
 
 function numeroAleatorio(min, max)
 {
@@ -14,6 +14,4 @@ function abrir()
     caminho = '../imagens/doors/door' + aleatorio + '.jpg';
 
     portaPrincipal.setAttribute('src', caminho);
-
-    var rs = portaPrincipal.getAttribute('src');
 }
