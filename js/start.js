7a1c2e7dbf445fe566ce55b39a889af73ed89af5
Paulo de Tasso <paulovendascontato@gmail.com>
2021-12-28 17:09:34 -0300

    28-12-2021 - 1709

--- js/start.js
+++ js/start.js
@@ -24,4 +24,58 @@ function retornarImagem()
     somVento.pause();
 }
 
+function setCookie(nome, valor, dias)
+{
+    diasms = (new Date()).getTime() + 1000 * 3600 * 24 * dias;
+    dias = new Date(diasms);
+    expires = dias.toGMTString();
+    document.cookie = escape(nome) + "=" + escape(valor) + "; expires=" + expires;
+}
 
+var online;
+
+contagemRegresiva();
+
+function contagemRegresiva()
+{
+    var ativar = setTimeout("visitando()", 300000);
+}
+
+function visitando()
+{
+    online += 1;
+
+    var visitasdoSite = document.getElementById('visitasSite');
+
+    if (!document.cookie)
+    {
+        setCookie("cookie", "1", 365);
+        var visitas = ("<font face='verdana' size='1'>Visitas : 1</font>");
+        visitasdoSite.innerHTML = visitas;
+    } else
+    {
+        var cont = document.cookie;
+        var dividindo = cont.split("=");
+        //document.write(dividindo[1]);
+        var numero = parseInt(dividindo[1]);
+        var soma = numero + 1;
+        var visitas = ("<font face='verdana' size='1'> Visitas: " + soma + "</font>");
+        setCookie("cookie", soma, 365);
+        visitasdoSite.innerHTML = visitas;
+    }
+
+    contagemRegresiva();
+
+}
+
+/*
+function contagemRegresiva()
+{
+    var t = setTimeout("visitando", 1000);  
+}
+
+function visitando()
+{
+    soma = soma +1;  
+}
+*/
