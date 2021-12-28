8fac0316ad93c75dca8bde0952fdc799ac4ea0bd
Paulo de Tasso <paulovendascontato@gmail.com>
2021-12-28 12:20:35 -0300

    28-12-2021 - 1220

--- js/start.js
+++ js/start.js
@@ -2,15 +2,6 @@ var imagem = document.getElementById('start')
 
 var numeroAleatorioFraseInterno;
 
-var frase0 = "0";
-var frase1 = "1";
-var frase2 = "2";
-var frase3 = "3";
-var frase4 = "4"
-var frase5 = "5"
-
-var frases = new Array(frase0, frase1, frase2, frase3, frase4, frase5);
-
 function numeroAleatorioFrase(min, max)
 {
     numeroAleatorioFraseInterno = Math.floor(Math.random() * (max - min) + min);
@@ -19,16 +10,12 @@ function numeroAleatorioFrase(min, max)
 function alterarImagem()
 {
     numeroAleatorioFrase(0, 6);
-    imagem.setAttribute('src', '/imagens/start' + numeroAleatorioFraseInterno + 'min.png')
+    imagem.setAttribute('src', 'imagens/start' + numeroAleatorioFraseInterno + 'min.png')
 }
 
 function retornarImagem()
 {
-    imagem.setAttribute('src', '/imagens/start0min.png')
+    imagem.setAttribute('src', 'imagens/start0min.png')
 }
 
-function contagemRegresiva()
-{
-    var t = setTimeout("alterarImagem()", 3000);
-}
 
