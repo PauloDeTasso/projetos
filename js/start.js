c0f4b5c03a6582b9abea3134e9917f2ab3870328
Paulo de Tasso <paulovendascontato@gmail.com>
2021-12-28 15:51:40 -0300

    28-12-2021 - 1551

--- js/start.js
+++ js/start.js
@@ -2,6 +2,10 @@ var imagem = document.getElementById('start')
 
 var numeroAleatorioFraseInterno;
 
+var somVento = document.getElementById('botaoEntrar');
+
+var visitasdoSite = document.getElementById('visitasSite')
+
 function numeroAleatorioFrase(min, max)
 {
     numeroAleatorioFraseInterno = Math.floor(Math.random() * (max - min) + min);
@@ -11,11 +15,13 @@ function alterarImagem()
 {
     numeroAleatorioFrase(0, 6);
     imagem.setAttribute('src', 'imagens/start' + numeroAleatorioFraseInterno + 'min.png')
+    somVento.play();
 }
 
 function retornarImagem()
 {
     imagem.setAttribute('src', 'imagens/start0min.png')
+    somVento.pause();
 }
 
 
