e83c870d2afa4b68ccce25aac9c630e1f00df40f
PauloDeTasso <93555337+PauloDeTasso@users.noreply.github.com>
2022-08-26 08:19:44 -0300

    26-08-2022 - 0819

--- testes/Lookalike3D/Lookalike3d.js
+++ testes/Lookalike3D/Lookalike3d.js
@@ -386,24 +386,24 @@ function showTime()
     let min = time.getMinutes();
     let sec = time.getSeconds();
     am_pm = "AM";
-
-    if (hour > 12)
-    {
-        hour -= 12;
-        am_pm = " || PM";
-    }
-    if (hour == 0)
-    {
-        hr = 12;
-        am_pm = " || AM";
-    }
-
-    hour = hour < 10 ? "0" + hour : hour;
-    min = min < 10 ? "0" + min : min;
-    sec = sec < 10 ? "0" + sec : sec;
-
+    /*
+        if (hour > 12)
+        {
+            hour -= 12;
+            am_pm = " || PM";
+        }
+        if (hour == 0)
+        {
+            hr = 12;
+            am_pm = " || AM";
+        }
+    *//*
+        hour = hour < 10 ? "0" + hour : hour;
+        min = min < 10 ? "0" + min : min;
+        sec = sec < 10 ? "0" + sec : sec;
+    */
     let currentTime = hour + ":"
-        + min + ":" + sec + am_pm;
+        + min + ":" + sec + "s || 3D";
 
     document.getElementById("relogio")
         .innerHTML = "Lookalike 3D || " + "\n" + currentTime;
