24f3e789942f6e2e1ebf6cf8b88bb5c14277d248
Paulo de Tasso <paulovendascontato@gmail.com>
2022-10-10 14:55:57 -0300

    10-10-2022 - 1455

--- Projetos/Lookalike3D/Lookalike3d.js
+++ Projetos/Lookalike3D/Lookalike3d.js
@@ -1313,6 +1313,44 @@ function itemMenuAnimacaoFinalizada()
     }
 }
 
+//
+
+function detectar_mobile()
+{
+    if (navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
+    )
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+if (detectar_mobile())
+{
+    if (screen.height > screen.width) 
+    {
+        imagemDeFundo.style.top = alturaJanela / 3.3 + "px";
+    } else
+    {
+        imagemDeFundo.style.top = "0px";
+    }
+
+} else
+{
+    imagemDeFundo.style.top = "0px";
+}
+
+//
+
 cabecalho.addEventListener('animationend', abrindoFechandoMenu, false);
 
 //secaoOptions.addEventListener('animationend', itemMenuAnimacaoFinalizada, false);
--- js/index.js
+++ js/index.js
@@ -347,6 +347,33 @@ function abrirPagina(link, target)
     window.open(link, target);
 }
 
+function detectar_mobile()
+{
+    if (navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
+    )
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+if (detectar_mobile())
+{
+
+} else
+{
+
+}
+
 /* COLORIDAS: 
 
 function luzes()
